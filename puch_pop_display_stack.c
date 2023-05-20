125958357a20629421d326bb49f117f618c405f4
Astro <abhisainiji46@gmail.com>
2023-05-20 13:02:28 +0530

    modified:   puch_pop_display_stack.c version 1.1

--- puch_pop_display_stack.c
+++ puch_pop_display_stack.c
@@ -8,10 +8,14 @@ void insertion();
 void deletion();
 void display();
 int main(){
-int input;
+int input,k;
 printf("Enter The Number Of Element In STACK\n");
 scanf("%d",&n);
 stack=(int *)malloc(n*sizeof(int));
+for(k=1;k<=n;k++){
+stack[k]=0;
+
+}
 do{
 printf("Enter The OPTION from The following\n");
 printf("1.INSERTING IN STACK\t2.DELETING FROM STACK\n3.DISPLAY THE SATCK\t4.EXIT THE PROGRAM\n");
@@ -38,9 +42,10 @@ printf("Thanks For Using This Program!!\n\n");
 return 0;}
 void insertion(){
     if(top<n){
-top=top+1;
-printf("Please Enter A Value For POSITION %d\n",top);
+
+printf("Please Enter A Value For POSITION %d\n",top+1);
 scanf("%d",&stack[top]);
+top=top+1;
     }
     else{
 printf("\nOOPS.....THE STACK IS FULL!!!\n");
@@ -52,8 +57,8 @@ if(top==0){
     printf("The Stack IS Empty!\n");
 }
 else{
-    printf("DATA");
-    stack[top]=NULL;
+    printf("Deleting DATA %d form LOC %d\n\n",stack[top],top);
+    stack[top]=0;
     top=top-1;
 
 }
